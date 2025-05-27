e6621eef6dfe00b43fb64ca8022791cf72a4dd21
Giancarlo Beristain Vazquez <giancarloberistain@gmail.com>
2025-05-27 18:27:17 -0400

    computer and human choices added

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function getComputerChoice(){
     const choices = ['rock', 'paper', 'scissors'];
-    const randomIndex = Math.floor(Math.random()* choices.lenghth);
+    const randomIndex = Math.floor(Math.random()* choices.length);
     return choices [randomIndex];
 }
+let choice;
+function getHumanChoice(){
+    choice = prompt("Enter rock, paper, or scissors").toLowerCase();
+}
+console.log(getHumanChoice());
+console.log(`You chose: ${choice}`);
